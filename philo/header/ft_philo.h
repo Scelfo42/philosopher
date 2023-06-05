bc8598ad852425d51856e6245b293fb8514e6880
Scelfo42 <rimscelfo.christian@gmail.com>
2023-06-05 20:16:28 +0200

    update

---
+++ philo/header/ft_philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_philo.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cscelfo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 11:06:23 by cscelfo           #+#    #+#             */
+/*   Updated: 2023/06/05 10:54:38 by cscelfo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PHILO_H
+# define FT_PHILO_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#endif
